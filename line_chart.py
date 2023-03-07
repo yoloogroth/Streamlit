14df8c3fa501a3cc4ed4db75107943ed0e05a041
yoloo.groth <zS20020311@estudiantes.uv.mx>
2023-03-07 10:15:25 -0600

    Date Set Bike

--- line_chart.py
+++ line_chart.py
@@ -3,9 +3,9 @@ import numpy as np
 import pandas as pd
 
 chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
+     np.random.randn(100 , 5),
      columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
 
-st.dataframe(chart_data)
+st.dataframe(chart_data) 
